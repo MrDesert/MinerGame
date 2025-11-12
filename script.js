4ab2065486be44acc4366debd6a47cc05d9f13b1
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2025-11-12 10:34:28 +0300

    alpha v0.02

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var hit = 1;
 var hp = 5;
 var hpCurrent = hp;
 var money = 0;
+var profit = 1;
 
 function hit_hp() {
     hpCurrent -= hit;
-    document.getElementById("hpID").innerHTML = hpCurrent;
     if (hpCurrent <= 0){
-        money++;
+        money += Math.floor(profit);
         document.getElementById("moneyID").innerHTML = money;
+        hp *= 2;
+        profit *= 1.5; 
         hpCurrent = hp;
     }
+    document.getElementById("hpID").innerHTML = hpCurrent;
 }
