322e9c8b476bb5af6afa863409e43d5caa980f3f
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2025-11-12 10:12:41 +0300

    v0.01

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var hit = 1;
 var hp = 5;
+var hpCurrent = hp;
+var money = 0;
 
 function hit_hp() {
-    hp -= hit;
-    document.getElementById("hpID").innerHTML = hp;
+    hpCurrent -= hit;
+    document.getElementById("hpID").innerHTML = hpCurrent;
+    if (hpCurrent <= 0){
+        money++;
+        document.getElementById("moneyID").innerHTML = money;
+        hpCurrent = hp;
+    }
 }
