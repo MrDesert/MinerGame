dd42f17178ddbc482f20e33c2d4abf54bdcece49
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2025-11-13 09:47:42 +0300

    alpha v0.13

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ var hpMinusOnePercentCost = 1;
 var depthLevel = 0;
 var profitUpCost = 1;
 var profitRatio = 2;
+var costOfPumpCost = 1;
+var costOfPumpRatio = 2;
 
 function moneyChanges(m){
     money += m;
     document.getElementById("moneyID").innerHTML = money;
-    var disabledIDs = ["hitPlusOneID", "hpMinusOnePercentID", "profitUpID"];    //ID которые надо включать и выключать в соответствии с количествой денег
-    var disabled = [hitPlusOneCost, hpMinusOnePercentCost, profitUpCost];       // переменные по которым отслеживать включение и выключение
+    var disabledIDs = ["hitPlusOneID", "hpMinusOnePercentID", "profitUpID", "costOfPumpID"];    //ID которые надо включать и выключать в соответствии с количествой денег
+    var disabled = [hitPlusOneCost, hpMinusOnePercentCost, profitUpCost, costOfPumpCost];       // переменные по которым отслеживать включение и выключение
     for (let i = 0; i<disabled.length; i++){
         if (money >= disabled[i]){
             document.getElementById(disabledIDs[i]).removeAttribute("disabled");
@@ -37,39 +39,52 @@ function hit_hp() {
 
 function hitPlusOne() {
     if (money >= hitPlusOneCost){
-        moneyChanges(-hitPlusOneCost);
+        moneyChanges(-Math.floor(hitPlusOneCost));
         hit++;
-        hitPlusOneCost *= 2;
+        hitPlusOneCost *= costOfPumpRatio;
         updateInfo();
     }
 }
 
 function hpMinusOnePercent(){
     if (money >= hpMinusOnePercentCost){
-        moneyChanges(-hpMinusOnePercentCost);
+        moneyChanges(-Math.floor(hpMinusOnePercentCost));
         hp = hp*0.99;
         hpCurrent *= 0.99; 
-        hpMinusOnePercentCost *= 2;
+        hpMinusOnePercentCost *= costOfPumpRatio;
         updateInfo();
     }
 }
 
 function profitUp(){
     if (money >= profitUpCost){
-        moneyChanges(-profitUpCost);
+        moneyChanges(-Math.floor(profitUpCost));
         profitRatio = profitRatio*1.01;
         profit *= 1.01;
-        profitUpCost *= 2;
+        profitUpCost *= costOfPumpRatio;
         updateInfo();
     }
 } 
+
+function costOfPump(){
+    if (money >= costOfPumpCost){
+        moneyChanges(-Math.floor(costOfPumpCost));
+        costOfPumpRatio = costOfPumpRatio*0.99;
+        costOfPumpCost *= costOfPumpRatio;
+        profitUpCost *= 0.99;
+        hpMinusOnePercentCost *= 0.99;
+        hitPlusOneCost *= 0.99;
+        updateInfo();
+    }
+}
 
 function updateInfo(){
     document.getElementById("profitID").innerHTML = Math.floor(profit);
     document.getElementById("depthLevelID").innerHTML = depthLevel;
     document.getElementById("hpID").innerHTML = Math.floor(hpCurrent);
-    document.getElementById("hitPlusOneCostID").innerHTML = hitPlusOneCost;
+    document.getElementById("hitPlusOneCostID").innerHTML = Math.floor(hitPlusOneCost);
     document.getElementById("hitID").innerHTML = "Удар " + hit; 
-    document.getElementById("hpMinusOnePercentCostID").innerHTML = hpMinusOnePercentCost;
-    document.getElementById("profitUpCostID").innerHTML = profitUpCost;
+    document.getElementById("hpMinusOnePercentCostID").innerHTML = Math.floor(hpMinusOnePercentCost);
+    document.getElementById("profitUpCostID").innerHTML = Math.floor(profitUpCost);
+    document.getElementById("costOfPumpCostID").innerHTML = Math.floor(costOfPumpCost);
 }
