0001b4b47cf43bb4d0051d493b79a9e79d80117e
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-01-10 15:02:02 +0500

    beta v0.3.0.1

--- math.js
+++ math.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 function mathTriangularNumber(N){
     return N * (N+1)/2;
 }
-=======
-function mathTriangularNumber(number){
-    return number * (number+1)/2;
-}
->>>>>>> 0ea064eb9af4c76faa6aff1881d0ce8bffba1b49
